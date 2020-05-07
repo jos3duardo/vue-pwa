99ce61668e0e1497ccfbf6e87aa4d4e9608a5784
jos3duardo <jos3duardolopes@gmail.com>
2020-05-07 19:49:22 -0400

    add files in cache

--- example/service-worker.js
+++ example/service-worker.js
@@ -1,5 +1,18 @@
+let cacheName = 'conhecendo-service-workers';
+let filesToCache = [
+  '/',
+  'index.html',
+  'style.css'
+]
+
+
 self.addEventListener('install', function (e) {
   console.log('[ServiceWorker] Installer')
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log('[ServiceWorker] Caching app shell');
+    })
+  )
 });
 
 self.addEventListener('activate', function (e) {
