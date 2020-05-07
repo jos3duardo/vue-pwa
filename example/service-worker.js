1eb369c162e890808958c4d005b79e22c98ebbe1
jos3duardo <jos3duardolopes@gmail.com>
2020-05-07 19:55:43 -0400

    leaving application available offline

--- example/service-worker.js
+++ example/service-worker.js
@@ -5,12 +5,13 @@ let filesToCache = [
   'style.css'
 ]
 
-
+//save files in cache
 self.addEventListener('install', function (e) {
   console.log('[ServiceWorker] Installer')
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
       console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
     })
   )
 });
@@ -19,6 +20,12 @@ self.addEventListener('activate', function (e) {
   console.log('[ServiceWorker] Activate')
 });
 
+//if not network
 self.addEventListener('fetch', function (e) {
   console.log('[ServiceWorker] Fetch', e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+        return response || fetch(e.request)
+    })
+  )
 });
