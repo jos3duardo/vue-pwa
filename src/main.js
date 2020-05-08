a28029e0ccf063b4e4a5e045387ebbf357a59d72
jos3duardo <jos3duardolopes@gmail.com>
2020-05-07 20:59:18 -0400

    create file push and import vueFire and firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import VueFire from 'vuefire'
+import firebase from 'firebase'
+import push from './push'
+
+Vue.use(VueFire)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
