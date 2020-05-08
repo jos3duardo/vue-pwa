5d0e7a4b1968bb84aa377e7d0a47b527432d142e
jos3duardo <jos3duardolopes@gmail.com>
2020-05-07 21:04:24 -0400

    create a project in firebase and start on vue

--- src/main.js
+++ src/main.js
@@ -10,6 +10,20 @@ import push from './push'
 
 Vue.use(VueFire)
 
+// Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyCzlWtZ84bYfxHIAC8J10GSLrXsP8YFC-A",
+  authDomain: "vue-pwa-6f8c4.firebaseapp.com",
+  databaseURL: "https://vue-pwa-6f8c4.firebaseio.com",
+  projectId: "vue-pwa-6f8c4",
+  storageBucket: "vue-pwa-6f8c4.appspot.com",
+  messagingSenderId: "237517305951",
+  appId: "1:237517305951:web:11aa4b4ecede8367f003ea",
+  measurementId: "G-2BXPHSLM13"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
